3fa952b20dbdef01fbbd4f06b1730900302a9069
naren09-tech <84986207+naren09-tech@users.noreply.github.com>
2021-12-25 13:10:54 +0530

    Add files via upload

---
+++ quiz_game_login.js
@@ -0,0 +1,9 @@
+var player1name;
+var player2name;
+function addUser(){
+    player1name=document.getElementById("player1_name").value;
+    localStorage.setItem("player1name",player1name);
+    player2name=document.getElementById("player2_name").value;
+    localStorage.setItem("player2name",player2name);
+    window.location="quiz_game_page.html";
+}
